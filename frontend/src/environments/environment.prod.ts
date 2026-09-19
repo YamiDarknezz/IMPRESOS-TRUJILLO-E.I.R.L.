e4555678305ccdca69ecca2df8aeb3a5b0833821
YamiDarknezz <erick.plasencia54@gmail.com>
2026-09-18 22:48:30 -0500

    chore(docker): agregar Dockerfile de frontend con Nginx y configurar stack completo en Docker Compose

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // CAMBIAR al desplegar: URL pública del backend (Cloud Run).
+  // CAMBIAR al desplegar: URL pública del backend (VPS particular propio).
   apiUrl: 'http://localhost:8000',
 };
